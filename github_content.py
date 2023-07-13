23b9f44703213b6e0b567c792f2269c4f7f95fa0
omar55221 <omar91010@yahoo.com>
2023-07-13 19:43:54 +0000

    tok

--- github_content.py
+++ github_content.py
@@ -8,7 +8,7 @@ def print_dir_contents(repo, path, file):
         else:
             file.write(f"https://github.com/{repo.full_name}/blob/{repo.default_branch}/{content.path}\n")
 
-g = Github("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")
+g = Github("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")
 repo = g.get_repo("omar55221/checkpoint2")
 
 with open('github_urls.txt', 'w') as f:
